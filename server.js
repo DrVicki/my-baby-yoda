aebd6791ff93ec51599976b8c9805ec461d5dc8c
Vicki Bealman <drbealman@gmail.com>
2021-03-07 17:04:28 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,93 @@
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true}))
+app.use(bodyParser.json())
+
+app.use(express.static('public'))
+
+const MongoClient = require('mongodb').MongoClient;
+
+MongoClient.connect('mongodb+srv://babyyoda:Danav1ck1*@cluster0.wxr4m.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+    useUnifiedTopology: true })
+        .then(client => {
+    console.log('Connected to Database')
+    const db = client.db('star-wars-quotes')
+    const quotesCollection = db.collection('quotes')
+
+    app.post('/quotes', (req, res) => {
+        quotesCollection.insertOne(req.body)
+        .then(result => {
+          res.redirect('/')
+        })
+        .catch(error => console.error(error))
+        })
+
+    app.set('view engine', 'ejs')
+
+
+
+    app.put('/quotes', (req, res) => {
+        quotesCollection.findOneAndUpdate(
+            { name: 'Yoda' },
+            {
+                $set: {
+                    name: req.body.name,
+                    quote: req.body.quote
+                }
+            },
+            {
+                upsert: true
+            }
+        )
+        .then(result => {
+            res.join('Success')
+        })
+        .catch(error => console.error(error))
+    })
+
+ 
+
+
+    app.get('/', (req, res) => {
+        const cursor = db.collection('quotes').find().toArray()
+        .then(results => {
+            res.render('index.ejs', { quotes: results })
+        })
+        .catch(error => console.error(error))
+       
+      
+        })
+  })
+  
+  app.delete('/quotes', (req, res) => {
+      quotesCollection.deleteOne(
+          { name: req.body.name },
+      )
+      .then(result => {
+          if (result.deletedCount === 0) {
+              return res.json('No quote to delete')
+          }
+          res.json(`Deleted Darth Vader's Quote`)
+      })
+      .catch(error => console.error(error))
+  })
+  
+
+
+  
+
+  
+  app.listen(3000, function() {
+      console.log('listening on 3000')
+  })
+
+
+
+
+ 
+
+        
